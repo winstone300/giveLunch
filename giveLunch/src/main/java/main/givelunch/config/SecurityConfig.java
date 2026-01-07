0b86e7c3992030c2312a23be155c5ed43e67a4e6
winstone300 <winstone300@navere.com>
2026-01-07 18:04:51 +0900

    security

---
+++ giveLunch/src/main/java/main/givelunch/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package main.givelunch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login/login","/login", "/signup", "/css/**", "/js/**", "/images/**","/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")    // GET /login -> 내가 만든 페이지
+                        .loginProcessingUrl("/login")   // POST /login -> 시큐리티가 처리(중요)
+                        .defaultSuccessUrl("/", true) // 성공 시 이동
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                );
+
+        return http.build();
+    }
+}
---
+++ giveLunch/src/main/java/main/givelunch/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package main.givelunch.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
