3b9ef52f298c71bf8c22fbb90112febfd804502d
winstone300 <winstone300@navere.com>
2026-01-08 14:55:38 +0900

    fix: thymeleaf 의존성 추가

--- giveLunch/src/main/java/main/givelunch/controllers/LoginController.java
+++ giveLunch/src/main/java/main/givelunch/controllers/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/login")
     public String login() {
-        return "login";
+        return "login/login";
     }
 
     @GetMapping("/main")
