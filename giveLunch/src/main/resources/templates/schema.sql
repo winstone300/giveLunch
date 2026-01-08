7e42f7d33c2cfd77b46e3546bac0f59681f25c66
winstone300 <winstone300@navere.com>
2026-01-08 17:51:50 +0900

    fix: create table error

--- giveLunch/src/main/resources/templates/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username varchar(50) not null ,
-    password varchar(255) not null,
-    email varchar(50) not null
-)
